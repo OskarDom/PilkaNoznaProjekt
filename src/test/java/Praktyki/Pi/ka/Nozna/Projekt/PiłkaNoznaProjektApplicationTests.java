78ba59babbd095868bb4a4217ef938f4960775cc
Oskar <oskardomanski0912@gmail.com>
2025-04-30 11:22:14 +0200

    Initial commit

---
+++ src/test/java/Praktyki/Pi/ka/Nozna/Projekt/PiłkaNoznaProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package Praktyki.Pi.ka.Nozna.Projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiłkaNoznaProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
